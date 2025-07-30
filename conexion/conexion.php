8ef0af3f233b2621793e2219ce01d32e9199f4b8
zaptersz <mauri3193.0@hotmail.com>
2025-07-30 11:50:41 -0400

    Create conexion.php

---
+++ conexion/conexion.php
@@ -0,0 +1,23 @@
+<?php
+class Conexion{
+    
+    function conecionBD(){
+        //datos de la BD
+        $host ="localhost";
+        $dbname = "mantenBodegas";
+        $username = "postgres";
+        $pasword = "1234";
+
+        try{
+            //Intento conectarme a la BD
+            $conn = new PDO ("pgsql:host=$host, dbname=$dbname", $username, $pasword);
+        }catch( PDOException $exp){
+            //error
+            echo ("No Se puede conectar, $exp");
+        }
+        //Devuelve conexion
+        return $conn;
+    }
+}
+
+?>
